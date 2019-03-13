87d873ba22814ec73257ad16546baf2a583bacee
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-03-13 23:06:46 +0100

    adds selinium webdriver and chromedriver gems, fixes indentation in haml file

--- Gemfile
+++ Gemfile
@@ -22,6 +22,8 @@ group :development, :test do
   gem 'shoulda-matchers'
   gem 'cucumber-rails', require: false
   gem 'database_cleaner'
+	gem 'chromedriver-helper'
+	gem 'selenium-webdriver'
 end
 
 group :development do
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,9 +1,30 @@
 require 'cucumber/rails'
 ActionController::Base.allow_rescue = false
 begin
-  DatabaseCleaner.strategy = :transaction
+	DatabaseCleaner.strategy = :transaction
 rescue NameError
-  raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
+	raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
 end
+
+Chromedriver.set_version '2.42'
+
+chrome_options = %w[headless disable-popup-blocking disable-infobars]
+
+chrome_options << 'auto-open-devtools-for-tabs'
+
+Capybara.register_driver :chrome do |app|
+	options = Selenium::WebDriver::Chrome::Options.new(
+		args: chrome_options
+		)
+		Capybara::Selenium::Driver.new(
+			app,
+			browser: :chrome,
+			options: options
+			)
+end
+
 Cucumber::Rails::Database.javascript_strategy = :truncation
 World(FactoryBot::Syntax::Methods)
+
+Capybara.server = :puma
+Capybara.javascript_driver = :chrome
