f97eb14cbd2ce261d419fea2c1fc036cd19c85da
vick3d <viktoro0090@gmail.com>
2019-03-14 16:12:58 +0100

    adds category model, creates reference in articles, adds associations, adds category links on index page

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
     validates :title, presence: true, length: { minimum: 5}
-    validates :content, presence: true, length: { minimum: 10}
+		validates :content, presence: true, length: { minimum: 10}
+		validates :category, presence: true
+		belongs_to :category
 end
