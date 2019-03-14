b968802408c90cb3cbf11631d41157f79c27cc00
vick3d <viktoro0090@gmail.com>
2019-03-14 16:33:36 +0100

    adds validation of category name

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,14 @@
 class Category < ApplicationRecord
-	validates :name, presence: true
 	has_many :articles
+	valid_categories = [
+		'sports',
+		'entertainment',
+		'local',
+		'global',
+		'tech',
+		'politics'
+	]
+	validates_inclusion_of :name, :in => valid_categories
+	validates :name, presence: true
+	validates_uniqueness_of :name
 end
