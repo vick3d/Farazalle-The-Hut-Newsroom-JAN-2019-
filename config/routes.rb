63fb1cf66c4c522c28c4a976199ab14fca708e4a
Laura Reale <laaru1988@gmail.com>
2019-03-15 17:08:18 +0100

    fix spacing problems, fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
 	root controller: :articles, action: :index
-	resources :subscriptions, only: [:index, :show, :create]
+	resources :articles, only: [:index, :show]
+	resources :subscriptions, only: [:create]
 end
