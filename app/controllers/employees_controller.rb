ca786a169c3f6a0b6e8625b469ac05f0b7c75379
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-03-18 15:19:33 +0100

    edits routes and fixes formatting in controller

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,4 +1,4 @@
 class EmployeesController < ApplicationController
 	def index
-  end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'employees/index'
 	devise_for :users
 
 	root controller: :articles, action: :index
 	resources :articles, only: [:index, :show]
+	resources :employees, only: [:index]
 end
