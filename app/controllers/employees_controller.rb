8d4803fcacbfe475cf885ce4ff203fd624c64334
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-03-19 11:56:57 +0100

    changed user role to integer, adds staff auth check so that onyl staff can view employee page

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,4 +1,14 @@
 class EmployeesController < ApplicationController
+	before_action :check_staff_auth
 	def index
+	end
+
+	private
+
+	def check_staff_auth
+		if user_signed_in? && current_user.role >= 2
+		else
+			redirect_to root_path
+		end
 	end
 end
